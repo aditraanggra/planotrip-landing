8138d1a726aa8fce1a0bbb409ae4c89ad21c13d4
Anggra Aditra <aditra.anggra@gmail.com>
2021-06-17 15:40:11 +0700

    add social icon

--- src/parts/Footer.js
+++ src/parts/Footer.js
@@ -1,6 +1,13 @@
 import React from 'react'
 
-import { FaAndroid, FaApple } from 'react-icons/fa'
+import {
+  FaAndroid,
+  FaApple,
+  FaYoutube,
+  FaInstagramSquare,
+  FaTwitter,
+  FaFacebook,
+} from 'react-icons/fa'
 
 import Button from '../components/Button'
 import Logo from './Logo'
@@ -17,14 +24,28 @@ function Footer() {
             <p>debbie.baker@example.com</p>
           </div>
         </div>
-        <div className='footer-row--wrapper'>
+        <div className='footer-col--wrapper'>
           <h3 className='footer-sub--title'>Social Media</h3>
           <div className='divider'></div>
+          <div className='footer-row--wrapper space-x-4 pt-6 mt-4'>
+            <Button type='link' isExternal className='social-icon'>
+              <FaFacebook />
+            </Button>
+            <Button type='link' isExternal className='social-icon'>
+              <FaInstagramSquare />
+            </Button>
+            <Button type='link' isExternal className='social-icon'>
+              <FaTwitter />
+            </Button>
+            <Button type='link' isExternal className='social-icon'>
+              <FaYoutube />
+            </Button>
+          </div>
         </div>
         <div className='footer-col--wrapper'>
           <h3 className='footer-sub--title'>Download Our App</h3>
           <div className='divider mb-4'></div>
-          <div className='space-y-4'>
+          <div className='space-y-4 mt-4'>
             <Button
               type='link'
               href='/'
@@ -43,7 +64,7 @@ function Footer() {
         </div>
       </div>
       <div className='footer-bottom'>
-        <p>2021&copy;- planotrip.com </p>
+        <p>Copyright &copy; 2021 - planotrip.com </p>
       </div>
     </footer>
   )
